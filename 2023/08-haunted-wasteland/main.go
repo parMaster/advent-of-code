14c8236c8d84ca1e84a9488f4240f13f94e8c619
parMaster <tony.racomore@gmail.com>
2023-12-08 14:23:21 +0200

    2023 day 8 - naive first part solution

---
+++ 2023/08-haunted-wasteland/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+var route string
+var m map[string][2]string
+
+func main() {
+	input, _ := os.ReadFile("../aoc-inputs/2023/08/input1.txt")
+	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
+
+	route = lines[0]
+	m := map[string][2]string{}
+	for _, l := range lines[2:] {
+		m[l[0:3]] = [2]string{l[7:10], l[12:15]}
+	}
+
+	i := 0
+	c := "AAA" // current, start
+	dir := map[rune]int{'L': 0, 'R': 1}
+	for {
+		for _, move := range []rune(route) {
+			c = m[c][dir[move]]
+			i++
+			if c == "ZZZ" {
+				break
+			}
+		}
+		if c == "ZZZ" {
+			break
+		}
+	}
+	fmt.Println("Part One:", i) // 12737
+}

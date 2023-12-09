a824700767d80aaeea7c805d414cef219f53de79
parMaster <tony.racomore@gmail.com>
2023-12-09 23:56:06 +0200

    day 9 test

--- 2023/09-mirage-maintenance/main_test.go
+++ 2023/09-mirage-maintenance/main_test.go
@@ -11,3 +11,8 @@ func TestPredict(t *testing.T) {
 	require.Equal(t, 28, predict([]int{1, 3, 6, 10, 15, 21}))
 	require.Equal(t, 68, predict([]int{10, 13, 16, 21, 30, 45}))
 }
+
+func TestSolve(t *testing.T) {
+	require.Equal(t, 1921197370, solve("../aoc-inputs/2023/09/input.txt", predict))
+	require.Equal(t, 1124, solve("../aoc-inputs/2023/09/input.txt", predict_past))
+}

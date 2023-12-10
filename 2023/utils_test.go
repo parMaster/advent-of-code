014c65105e3ab6f0bdf4242fa621f4482791f4cd
parMaster <tony.racomore@gmail.com>
2023-12-10 03:36:45 +0200

    All, AllEqual, AllSame, AllFunc

--- 2023/utils_test.go
+++ 2023/utils_test.go
@@ -63,6 +63,55 @@ func abs(n int64) int64 {
 	return n
 }
 
+func AllEqual(a []int, s int) bool {
+	for i := range a {
+		if a[i] != s {
+			return false
+		}
+	}
+	return true
+}
+
+func AllSame(a []int) bool {
+	for i := 1; i < len(a); i++ {
+		if a[i] != a[0] {
+			return false
+		}
+	}
+	return true
+}
+
+func AllFunc[V any](a []V, f func(V) bool) bool {
+	for i := 0; i < len(a); i++ {
+		if !f(a[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+func Test_All(t *testing.T) {
+	require.True(t, AllEqual([]int{0, 0, 0, 0, 0}, 0))
+	require.False(t, AllEqual([]int{1, 0, 0, 0, 0}, 0))
+	require.False(t, AllEqual([]int{0, 1, 0, 0, 0}, 0))
+	require.False(t, AllEqual([]int{1}, 0))
+	require.True(t, AllEqual([]int{0}, 0))
+
+	require.True(t, AllSame([]int{0}))
+	require.True(t, AllSame([]int{0, 0}))
+	require.False(t, AllSame([]int{0, 1}))
+
+	require.True(t, AllFunc([]int{5, 5, 5}, func(a int) bool { return a == 5 }), "every element of slice equals 5")
+	require.False(t, AllFunc([]int{5, 5, 1}, func(a int) bool { return a == 5 }), "every element of slice equals 5")
+
+	require.True(t, AllFunc([]int{2, 4, 6, 224}, func(a int) bool { return a%2 == 0 }), "Multiple of 2")
+	require.False(t, AllFunc([]int{2, 4, 6, 224}, func(a int) bool { return a%3 == 0 }), "but not all of them are multiple of 3")
+	require.True(t, AllFunc([]int{48, 49, 50, 51, 52, 53, 54, 55, 56, 57}, func(a int) bool { return a-0x30 >= 0 && a-0x30 <= 9 }), "digits (0-9) in ASCII?")
+
+	require.True(t, AllFunc([][2]string{{"oh", "yes"}, {"oh", "yes"}}, func(e [2]string) bool { return e[1] == "yes" }), "generic AllFunc")
+	require.False(t, AllFunc([][2]string{{"oh", "yes"}, {"oh", "no"}}, func(e [2]string) bool { return e[1] == "yes" }), "generic AllFunc")
+}
+
 func Test_EatMemory(t *testing.T) {
 	n := 300 * 1000 * 1000
 	// n = 3603973818

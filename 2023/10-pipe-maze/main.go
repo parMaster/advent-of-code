2d471f4fe1370db9a8d93fee7250a993a62eac14
parMaster <tony.racomore@gmail.com>
2023-12-11 15:32:34 +0200

    day 10.. it's long and ugly and vomits visuals, but it works

--- 2023/10-pipe-maze/main.go
+++ 2023/10-pipe-maze/main.go
@@ -26,8 +26,12 @@ var ps = map[rune][]image.Point{
 	'S': xyDir,
 }
 
-func read(in string) (map[image.Point]rune, map[image.Point]int, image.Point) {
-	m, v := make(map[image.Point]rune), make(map[image.Point]int)
+type maze map[image.Point]rune
+
+type loop map[image.Point]int
+
+func read(in string) (maze, image.Point) {
+	m := make(map[image.Point]rune)
 	start := image.Point{0, 0}
 	for y, l := range strings.Split(strings.TrimSpace(string(in)), "\n") {
 		for x, r := range strings.TrimSpace(l) {
@@ -37,8 +41,7 @@ func read(in string) (map[image.Point]rune, map[image.Point]int, image.Point) {
 			}
 		}
 	}
-	v[start] = 1
-	return m, v, start
+	return m, start
 }
 
 func connects(m map[image.Point]rune, from, to image.Point) bool {
@@ -50,8 +53,14 @@ func connects(m map[image.Point]rune, from, to image.Point) bool {
 	return false
 }
 
-func solve(f string) int {
-	m, v, pos := read(f)
+func p1(f string) int {
+	m, pos := read(f)
+	p1, _ := giantLoop(m, pos)
+	return p1
+}
+
+func giantLoop(m maze, pos image.Point) (int, loop) {
+	var v loop = loop{pos: 1}
 	for {
 		steps := ps[m[pos]]
 		for _, step := range steps {
@@ -60,8 +69,7 @@ func solve(f string) int {
 
 			if connects(m, pos, newPos) && connects(m, newPos, pos) {
 				if v[newPos] == 1 {
-					show(m, v)
-					return slices.Max(maps.Values(v)) / 2
+					return slices.Max(maps.Values(v)) / 2, v
 				}
 				if v[newPos] == 0 {
 					v[newPos] = v[pos] + 1
@@ -73,27 +81,165 @@ func solve(f string) int {
 	}
 }
 
-func main() {
-	in, _ := os.ReadFile("../aoc-inputs/2023/10/input.txt")
-	fmt.Println("Day 10: Pipe Maze")
-	fmt.Println("\tPart One:", solve(string(in))) // 7107
+func show(m map[image.Point]rune, v map[image.Point]int) {
+	mx, my := maxPoint(m)
+	fmt.Println(mx, "x", my, ":")
+	for y := 0; y <= my; y++ {
+		for x := 0; x <= mx; x++ {
+			if v[image.Pt(x, y)] == 0 {
+				fmt.Print(".")
+			} else {
+				fmt.Print(string(m[image.Pt(x, y)]))
+			}
+		}
+		fmt.Println()
+	}
+	fmt.Println()
 }
 
-func show(m map[image.Point]rune, v map[image.Point]int) {
-
-	mx, my := 0, 0
+func maxPoint[V any](m map[image.Point]V) (mx, my int) {
 	for p := range m {
 		mx = max(mx, p.X)
 		my = max(my, p.Y)
 	}
-	fmt.Println(mx, "x", my, ":")
+	return mx, my
+}
+
+func main() {
+	in, _ := os.ReadFile("../aoc-inputs/2023/10/input.txt")
+	fmt.Println("Day 10: Pipe Maze")
+	fmt.Println("\tPart One:", p1(string(in))) // 7107
+	fmt.Println("\tPart Two:", p2(string(in))) // 281
+}
+
+func p2(f string) int {
+	maze, start := read(f)
+	_, loop := giantLoop(maze, start)
+
+	repairedQuadLoop := repair(quad(maze), quad(loop))
+	showLoop(repairedQuadLoop)
+
+	mx, my := maxPoint(repairedQuadLoop)
+	// fill from each corner, quick and dirty
+	fill(&repairedQuadLoop, image.Pt(0, 0))
+	fill(&repairedQuadLoop, image.Pt(mx, 0))
+	fill(&repairedQuadLoop, image.Pt(0, my))
+	fill(&repairedQuadLoop, image.Pt(mx, my))
+
+	loop = shrink(repairedQuadLoop)
+
+	showLoop(loop)
+
+	return countZeroes(loop)
+}
+
+// quadruples image in size, surrounds every value with
+func quad[V int | rune](loop map[image.Point]V) map[image.Point]V {
+	quadLoop := make(map[image.Point]V)
+	mx, my := maxPoint(loop)
 	for x := 0; x <= mx; x++ {
 		for y := 0; y <= my; y++ {
-			if v[image.Pt(y, x)] == 0 {
-				fmt.Print(".")
-			} else {
-				fmt.Print(string(m[image.Pt(y, x)]))
+			quadLoop[image.Pt(x*2, y*2)] = loop[image.Pt(x, y)]
+			quadLoop[image.Pt(x*2+1, y*2)] = 0
+			quadLoop[image.Pt(x*2, y*2+1)] = 0
+			quadLoop[image.Pt(x*2+1, y*2+1)] = 0
+		}
+	}
+	return quadLoop
+}
+
+// connects indirectly, over one point
+func overConnects(m map[image.Point]rune, from, to image.Point) bool {
+	for _, s := range ps[m[from]] {
+		if from.Add(s).Add(s) == to {
+			return true
+		}
+	}
+	return false
+}
+
+// repairs quadrupled loop
+func repair(maze maze, loop loop) loop {
+	mx, my := maxPoint(loop)
+	for x := 0; x <= mx; x++ {
+		for y := 0; y <= my; y++ {
+
+			if overConnects(maze, image.Pt(x-1, y), image.Pt(x+1, y)) &&
+				overConnects(maze, image.Pt(x+1, y), image.Pt(x-1, y)) {
+				loop[image.Pt(x, y)] = 1
 			}
+
+			if overConnects(maze, image.Pt(x, y-1), image.Pt(x, y+1)) &&
+				overConnects(maze, image.Pt(x, y+1), image.Pt(x, y-1)) {
+				loop[image.Pt(x, y)] = 1
+			}
+
+		}
+	}
+	return loop
+}
+
+func fill(v *loop, start image.Point) {
+	val, ok := (*v)[start]
+	if !ok || val > 0 {
+		return
+	}
+	(*v)[start] = 1
+	for i := range xyDir {
+		candidate := start.Add(xyDir[i])
+		fill(v, candidate)
+	}
+}
+
+// shrink
+func shrink[V int | rune](loop map[image.Point]V) map[image.Point]V {
+	mx, my := maxPoint(loop)
+	for x := 0; x <= (mx-1)/2; x++ {
+		for y := 0; y <= (my-1)/2; y++ {
+			delete(loop, image.Pt(x*2+1, y*2))
+			delete(loop, image.Pt(x*2, y*2+1))
+			delete(loop, image.Pt(x*2+1, y*2+1))
+		}
+	}
+	return loop
+}
+
+// count zeroes
+func countZeroes(loop map[image.Point]int) int {
+	cnt := 0
+	mx, my := maxPoint(loop)
+	for y := 0; y <= my+1; y++ { // lines
+		for x := 0; x <= mx+1; x++ { // cols
+			v, ok := loop[image.Pt(x, y)]
+			if !ok {
+				continue
+			}
+			if v == 0 {
+				cnt++
+				continue
+			}
+		}
+	}
+
+	return cnt
+}
+
+func showLoop(loop map[image.Point]int) {
+	mx, my := maxPoint(loop)
+	fmt.Println("Loop: mx=", mx, " my=", my, ":")
+	fmt.Println()
+	for y := 0; y <= my+1; y++ { // lines
+		for x := 0; x <= mx+1; x++ { // cols
+			v, ok := loop[image.Pt(x, y)]
+			if !ok {
+				fmt.Print(" ")
+				continue
+			}
+			if v == 0 {
+				fmt.Print(".")
+				continue
+			}
+			fmt.Print("#")
 		}
 		fmt.Println()
 	}
--- 2023/10-pipe-maze/main_test.go
+++ 2023/10-pipe-maze/main_test.go
@@ -32,18 +32,69 @@ SJ.L7
 LJ...
 `
 
-func Test_Solve(t *testing.T) {
+var i200 = `
+...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
+`
+var i201 = `
+..........
+.S------7.
+.|F----7|.
+.||OOOO||.
+.||OOOO||.
+.|L-7F-J|.
+.|II||II|.
+.L--JL--J.
+..........
+`
 
-	m, _, _ := read(i1)
+var i202 = `
+.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+`
+
+func TestFuncs(t *testing.T) {
+	m, _ := read(i201)
+	w, h := maxPoint(m)
+	require.Equal(t, 9, w)
+	require.Equal(t, 8, h)
+
+	m, _ = read(i1)
 
 	require.True(t, connects(m, image.Pt(1, 1), image.Pt(1, 0)), "S ↑ L is technically connected")
 	require.False(t, connects(m, image.Pt(1, 0), image.Pt(1, 1)), "L ↓ S is not connected")
+}
 
-	require.Equal(t, 4, solve(i0))
-	require.Equal(t, 4, solve(i1))
-	require.Equal(t, 8, solve(i2))
+func Test_p1(t *testing.T) {
+	require.Equal(t, 4, p1(i0))
+	require.Equal(t, 4, p1(i1))
+	require.Equal(t, 8, p1(i2))
 
 	in, _ := os.ReadFile("../aoc-inputs/2023/10/input.txt")
-	require.Equal(t, 7107, solve(string(in)))
+	require.Equal(t, 7107, p1(string(in)))
+}
 
+func Test_p2(t *testing.T) {
+
+	require.Equal(t, 4, p2(i200))
+	require.Equal(t, 4, p2(i201))
+	require.Equal(t, 8, p2(i202))
+
+	in, _ := os.ReadFile("../aoc-inputs/2023/10/input.txt")
+	require.Equal(t, 281, p2(string(in)))
 }

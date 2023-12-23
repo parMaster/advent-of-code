9a4b4753ff13b98150cfb9771d3187668e426002
parMaster <tony.racomore@gmail.com>
2023-12-23 13:48:35 +0200

    day 20 part one

--- 2023/20-pulse-propagation/main.go
+++ 2023/20-pulse-propagation/main.go
@@ -65,7 +65,7 @@ type Module struct {
 type Modules map[string]Module
 
 func main() {
-	file := "../aoc-inputs/2023/20/input02.txt"
+	file := "../aoc-inputs/2023/20/input.txt"
 	in, _ := os.ReadFile(file)
 
 	modules := Modules{}
@@ -95,12 +95,25 @@ func main() {
 	var sums = Sums{low: 0, high: 0}
 	var startPulse = Pulse{from: "button", to: "roadcaster", pulse: low}
 	fmt.Println(startPulse.from, startPulse.pulse, startPulse.to)
-	for n := 0; n < 4; n++ {
+	for n := 0; n < 1000; n++ {
+		q := Queue{startPulse}
+		sums[startPulse.pulse]++
+		fmt.Println()
 
-		res := modules.send(startPulse)
-		sums[low] += res[low]
-		sums[high] += res[high]
+		for len(q) > 0 {
+			nq := Queue{}
 
+			for i := range q {
+				fmt.Println(q[i].from, q[i].pulse, q[i].to)
+				nq = append(nq, modules.call(q[i])...)
+			}
+
+			q = nq
+			for i, _ := range nq {
+				sums[nq[i].pulse]++
+				// fmt.Println(v.from, v.pulse, v.to)
+			}
+		}
 	}
 	fmt.Println(sums)
 	fmt.Println(sums[low] * sums[high])
@@ -110,18 +123,18 @@ var pulseLabel map[PulseType]string = map[PulseType]string{low: "->low->", high:
 
 type Sums map[PulseType]int
 
-func (m *Modules) send(p Pulse) Sums {
-	fmt.Println("Sending: ", p.from, pulseLabel[p.pulse], p.to)
-	var sums = Sums{low: 0, high: 0}
-	sums[p.pulse]++
+// pulse is received by p.to, p.to believes that state and inputs was changed before
+// so only pulses sent for every this.next module:
+// - ff module: next state changed + pulse returned
+// - con module: inputs changed + pulse returned
+func (m *Modules) call(p Pulse) []Pulse {
 
 	receiver := (*m)[p.to]
+	pulses := []Pulse{}
 
 	if receiver.action == broad {
 		for _, n := range receiver.next {
-			res := m.send(Pulse{p.to, n, p.pulse})
-			sums[low] += res[low]
-			sums[high] += res[high]
+			pulses = append(pulses, Pulse{p.to, n, p.pulse})
 		}
 	}
 
@@ -130,9 +143,7 @@ func (m *Modules) send(p Pulse) Sums {
 		(*m)[p.to] = receiver
 
 		for _, n := range receiver.next {
-			res := m.send(Pulse{p.to, n, high})
-			sums[low] += res[low]
-			sums[high] += res[high]
+			pulses = append(pulses, Pulse{p.to, n, PulseType(receiver.state)})
 		}
 	}
 
@@ -140,17 +151,15 @@ func (m *Modules) send(p Pulse) Sums {
 		receiver.inputs[p.from] = p.pulse
 		(*m)[p.to] = receiver
 
-		for _, sendPulse := range []PulseType{low, high} {
-			if !slices.Contains(maps.Values(receiver.inputs), sendPulse) { // all high
-				for _, n := range receiver.next {
-					res := m.send(Pulse{p.to, n, PulseType(flipState[State(sendPulse)])})
-					sums[low] += res[low]
-					sums[high] += res[high]
-				}
-			}
+		nextPulse := high
+		if !slices.Contains(maps.Values(receiver.inputs), low) {
+			nextPulse = low
 		}
 
+		for _, n := range receiver.next {
+			pulses = append(pulses, Pulse{p.to, n, nextPulse})
+		}
 	}
 
-	return sums
+	return pulses
 }

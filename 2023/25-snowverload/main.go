9286105371e57b8f93575f44e157bb153b1d117f
parMaster <tony.racomore@gmail.com>
2024-03-19 22:16:18 +0200

    multicore bf

--- 2023/25-snowverload/main.go
+++ 2023/25-snowverload/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
 	"strings"
 	"time"
 
+	"github.com/gammazero/workerpool"
 	"golang.org/x/exp/maps"
 )
 
@@ -30,6 +32,44 @@ func traverseOnce(g map[string][]string, source string, f func(currentNode strin
 			}
 		}
 	}
+}
+
+func checkConnections(g map[string][]string, nodes map[string]bool, c1v, c2v, c3v [2]string) (bool, int) {
+
+	// Deep copy of the graph
+	graph := map[string][]string{}
+	for k, v := range g {
+		for _, vv := range v {
+			if (k == c1v[0] && vv == c1v[1]) ||
+				(k == c2v[0] && vv == c2v[1]) ||
+				(k == c3v[0] && vv == c3v[1]) ||
+				(vv == c1v[0] && k == c1v[1]) ||
+				(vv == c2v[0] && k == c2v[1]) ||
+				(vv == c3v[0] && k == c3v[1]) {
+				continue
+			}
+			graph[k] = append(graph[k], vv)
+		}
+	}
+
+	flatGraph := maps.Clone(nodes)
+
+	island := 0
+	islanSize := map[int]int{}
+	for node, visited := range flatGraph {
+		if !visited {
+			island++
+			traverseOnce(graph, node, func(currentNode string) {
+				flatGraph[currentNode] = true
+				islanSize[island]++
+			})
+		}
+	}
+	if len(islanSize) == 2 {
+		fmt.Println(islanSize)
+		return true, islanSize[1] * islanSize[2]
+	}
+	return false, 0
 }
 
 func solve(filename string) (int, int) {
@@ -57,62 +97,50 @@ func solve(filename string) (int, int) {
 		}
 	}
 
-	// connList := maps.Keys(connections)
-	// slices.Sort(connList)
 	fmt.Println(len(connections))
+
+	tasks := make(chan int, (len(connections)-1)*(len(connections)-2))
+	result := make(chan int, 1)
+	cores := runtime.NumCPU()
+	wp := workerpool.New(cores)
 
 	iter := 0
 	for c1, c1v := range connections {
+		submitted := 0
+		gotResult := 0
 		for c2, c2v := range connections {
 			for c3, c3v := range connections {
 				if c1 != c2 && c1 != c3 && c2 != c3 {
-					iter++
-					if iter%1000 == 0 {
-						fmt.Println(iter)
-						// fmt.Println(c1v, c2v, c3v)
-					}
-					// fmt.Println(c1, c2, c3)
-
-					// Deep copy of the graph
-					graph := map[string][]string{}
-					for k, v := range g {
-						for _, vv := range v {
-							if (k == c1v[0] && vv == c1v[1]) ||
-								(k == c2v[0] && vv == c2v[1]) ||
-								(k == c3v[0] && vv == c3v[1]) ||
-								(vv == c1v[0] && k == c1v[1]) ||
-								(vv == c2v[0] && k == c2v[1]) ||
-								(vv == c3v[0] && k == c3v[1]) {
-								continue
-							}
-							graph[k] = append(graph[k], vv)
+					submitted++
+					wp.Submit(func() {
+						ok, res := checkConnections(g, nodes, c1v, c2v, c3v)
+						if ok {
+							gotResult = res
 						}
-					}
-
-					flatGraph := maps.Clone(nodes)
-
-					island := 0
-					islanSize := map[int]int{}
-					for node, visited := range flatGraph {
-						if !visited {
-							island++
-							traverseOnce(graph, node, func(currentNode string) {
-								flatGraph[currentNode] = true
-								islanSize[island]++
-							})
-						}
-					}
-					if len(islanSize) == 2 {
-						fmt.Println(islanSize)
-						return islanSize[1] * islanSize[2], 0
-					}
+						tasks <- 1
+					})
 				}
 
 			}
 		}
+		// wait for all tasks to finish before submitting next batch
+		for i := 0; i < submitted; i++ {
+			iter++
+			if iter%1000 == 0 {
+				fmt.Println(iter)
+			}
+			<-tasks
+		}
+		if gotResult > 0 {
+			result <- gotResult
+			break
+		}
 	}
-
-	return 0, 0
+	p1 := <-result
+	wp.Stop()
+	close(tasks)
+	close(result)
+	return p1, 0
 }
 
 func main() {

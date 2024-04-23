3f4f55c4ded0ce74de667942b7b227db0de2b6ad
Dmytro Borshchanenko <tony.racomore@gmail.com>
2024-04-23 13:51:38 +0300

    2020 day 6 is trivial

---
+++ 2020/06-custom-customs/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func solve(f string) (p1, p2 int) {
+
+	in, _ := os.ReadFile(f)
+	groups := strings.Split(strings.TrimSpace(string(in)), "\n\n")
+	for _, group := range groups {
+		m := map[rune]int{}
+		lines := strings.Split(group, "\n")
+		for _, line := range lines {
+			for _, c := range line {
+				m[c]++
+			}
+		}
+		p1 += len(m)
+		for _, ac := range m {
+			if ac == len(lines) {
+				p2++
+			}
+		}
+	}
+
+	return
+}
+
+func main() {
+	start := time.Now()
+	fmt.Println("Day 06: Custom Customs")
+	p1, p2 := solve("input.txt")
+	fmt.Println("\tPart One:", p1) //
+	fmt.Println("\tPart Two:", p2) //
+	fmt.Printf("Done in %.3f seconds \n", time.Since(start).Seconds())
+}

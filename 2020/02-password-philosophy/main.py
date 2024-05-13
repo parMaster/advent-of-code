551a51af750df19163aab60b3a32f8a05db04144
parmaster <tony.racomore@gmail.com>
2024-05-13 03:12:29 +0300

    there is XOR (^) in python...

---
+++ 2020/02-password-philosophy/main.py
@@ -0,0 +1,27 @@
+import time
+
+def solve():
+	p1, p2 = 0, 0
+	lines = open("input.txt", "r").read().splitlines()
+	for i in range(len(lines)):
+		parts = lines[i].split(":")
+		pwd = parts[1].strip(" ")
+		min, max = parts[0].split(" ")[0].split("-", 2)
+		# Part 1:
+		if  int(min) <= pwd.count(parts[0][-1]) <= int(max):
+			p1+=1
+		# Part 2:
+		if (pwd[int(min)-1] == parts[0][-1]) ^ (pwd[int(max)-1] == parts[0][-1]): # XOR
+			p2+=1
+
+	return p1, p2
+
+
+print("Day 02: Password Philosophy")
+start = time.time()
+p1, p2 = solve()
+print("\tPart 1: ", p1)
+print("\tPart 2: ", p2)
+print("Done in %s seconds" % round(time.time() - start, 3))
+
+

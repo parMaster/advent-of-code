43c285614baca959b77fe22c04b0473e41ba7b18
parmaster <tony.racomore@gmail.com>
2024-12-08 18:08:24 +0200

    2024 day 08

---
+++ 2024/08-Resonant-Collinearity/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"os"
+	"strings"
+	"time"
+)
+
+type Field map[image.Point]rune
+
+func ReadField(in string) (m Field, ant map[rune][]image.Point, w int, h int) {
+	m = make(map[image.Point]rune)
+	ant = make(map[rune][]image.Point)
+	for y, l := range strings.Split(strings.TrimSpace(string(in)), "\n") {
+		w = len(l) - 1
+		h = y
+		for x, r := range strings.TrimSpace(l) {
+			if r != '.' {
+				p := image.Point{x, y}
+				m[p] = r
+				t, ok := ant[r]
+				if ok {
+					t = append(t, p)
+				} else {
+					t = []image.Point{p}
+				}
+				ant[r] = t
+			}
+		}
+	}
+	return
+}
+
+func solve(file string) (p1, p2 int) {
+	in, _ := os.ReadFile(file)
+	f, ant, w, h := ReadField(string(in))
+
+	bounds := image.Rect(0, 0, w+1, h+1)
+	mp1 := map[image.Point]bool{}
+	mp2 := map[image.Point]bool{}
+	for _, ants := range ant {
+		for ai, a := range ants {
+			for _, pair := range ants[ai+1:] {
+				dist := a.Sub(pair)
+				for mul := range max(w, h) / 2 {
+					delta := dist.Mul(mul)
+					for _, pt := range []image.Point{
+						a.Add(delta), a.Sub(delta), pair.Add(delta), pair.Sub(delta),
+					} {
+						if mul == 1 && pt.In(bounds) && !pt.Eq(pair) && !pt.Eq(a) {
+							mp1[pt] = true
+						}
+						if pt.In(bounds) {
+							mp2[pt] = true
+						}
+					}
+				}
+			}
+		}
+	}
+	f.Show(w, h, mp2)
+
+	return len(mp1), len(mp2)
+}
+
+func main() {
+	start := time.Now()
+	fmt.Println("Day 08: Resonant Collinearity")
+	// p1, p2 := solve("input_pub.txt")
+	p1, p2 := solve("../aoc-inputs/2024/08/input.txt")
+	fmt.Println("\tPart One:", p1) // 261
+	fmt.Println("\tPart Two:", p2) // 898
+	fmt.Printf("Done in %.3f seconds \n", time.Since(start).Seconds())
+}
+
+// Show prints the Field
+func (f Field) Show(w, h int, mp map[image.Point]bool) {
+	fmt.Println(w+1, "x", h+1, ":")
+	for y := 0; y <= h; y++ {
+		for x := 0; x <= w; x++ {
+			if r, ok := f[image.Pt(x, y)]; ok {
+				fmt.Print(string(r))
+				continue
+			}
+			if _, ok := mp[image.Pt(x, y)]; ok {
+				fmt.Print("#")
+				continue
+			}
+			fmt.Print(".")
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+}
